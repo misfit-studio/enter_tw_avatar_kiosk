31e2463e9a8318e910b4b5b0dd39eac48310007d
Dario Breitenstein <dario@imakethings.ch>
2023-11-23 13:31:00 +0100

    add build stuff

---
+++ scripts/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# install flutter-pi
+sudo apt install cmake libgl1-mesa-dev libgles2-mesa-dev libegl1-mesa-dev libdrm-dev libgbm-dev ttf-mscorefonts-installer fontconfig libsystemd-dev libinput-dev libudev-dev  libxkbcommon-dev jq
+sudo fc-cache
+
+git clone https://github.com/ardera/flutter-pi
+cd flutter-pi
+
+mkdir build && cd build
+cmake ..
+make -j`nproc`
+
+sudo make install
+
+cd /home/pi
+git clone https://github.com/misfit-studio/enter_tw_avatar_kiosk.git
+sudo cp enter_tw_avatar_kiosk/bin/libspine_flutter.so /usr/lib
+
+cp enter_tw_avatar_kiosk/scripts/startup.sh /home/pi
+echo "/home/pi/startup.sh" >> /etc/rc.local
